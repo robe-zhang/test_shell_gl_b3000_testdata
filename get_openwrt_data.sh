b083439bd6504e94efec43670b07fc0b447ee8f9
robe.zhang <82796620@qq.com>
2024-02-09 04:02:36 +0800

    modify expect file

--- get_openwrt_data.sh
+++ get_openwrt_data.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/expect -f
 
-set password [lindex $argv 0]
+set timeout 1000
+set password_string [lindex $argv 0]
 set thermal_zone [lindex $argv 1]
 
 spawn ssh root@192.168.51.1 cat $thermal_zone
 expect "*password:"
-send "$password\n"
-interact
+send "$password_string\n"
+expect eof
--- get_openwrt_data2.sh
+++ get_openwrt_data2.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/expect -f
 
-set password [lindex $argv 0]
+set password_string [lindex $argv 0]
 
 spawn ssh root@192.168.51.1 free -h
 expect "*password:"
-send "$password\n"
-interact
+send "$password_string\n"
+expect eof
